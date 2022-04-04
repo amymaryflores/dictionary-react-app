0431714459533dd0ce5df89d17a9df14e06cb3ac
amymaryflores <amyflores125@gmail.com>
2022-04-04 18:42:08 -0400

    add

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ export default function App() {
                     <Dictionary defaultKeyword="Batman" />
                 </main>
                 <footer className="App-footer">
-                    Coded by {" "}
-                    <a href="mailto:tessehellena@gmail.com">Amy Flores</a> {" "},is open-sourced on {" "}
+                    Coded by 
+                   Amy Flores open-sourced on {" "}
                     <a href="https://github.com/amymaryflores/dictionary-react-app" target="_blank" rel="noreferrer"> GitHub</a> and hosted on {" "}
                     <a href="" target="_blank" rel="noreferrer">Netlify </a>.
                 </footer>

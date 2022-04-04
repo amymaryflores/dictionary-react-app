6887c81738921021c8b621688e6c635ac905ba11
amymaryflores <amyflores125@gmail.com>
2022-04-04 18:39:38 -0400

    add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import Dictionary from "./Dictionary";
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+ <div className="App">
+            <div className="container">
+                <header className="App-header">
+                </header>
+                <main>
+                    <Dictionary defaultKeyword="Batman" />
+                </main>
+                <footer className="App-footer">
+                    Coded by {" "}
+                    <a href="mailto:tessehellena@gmail.com">Amy Flores</a> {" "},is open-sourced on {" "}
+                    <a href="https://github.com/amymaryflores/dictionary-react-app" target="_blank" rel="noreferrer"> GitHub</a> and hosted on {" "}
+                    <a href="" target="_blank" rel="noreferrer">Netlify </a>.
+                </footer>
+            </div>
+        </div>
+    );
+  }
